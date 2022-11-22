804ffda51a59cf4c1a056ddf0b2e631f110c443c
karanmaurya15 <karanmaurya9663@gmail.com>
2022-11-22 20:21:56 +0530

    Emp Wage using Switch case is done

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -2,28 +2,27 @@
 {
     internal class Program
     {
+        public const int IS_FULL_TIME = 1;
+        public const int IS_PART_TIME = 2;
+        public const int EMP_RATE_PER_HOUR = 150;
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program\n");
-          
-            int IS_FULL_TIME = 1;
-            int IS_PART_TIME = 2;
-            int EMP_RATE_PER_HOUR = 150;
+            Console.WriteLine("Welcome to Employee Wage Computation Program\n");       
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();          
             int empCheck = random.Next(0, 3);
-            if (empCheck == IS_FULL_TIME)
+            switch (empCheck)
             {
-                empHrs = 8;
-            }
-            else if (empCheck == IS_PART_TIME)
-            {
-                empHrs = 4;
-            }
-            else
-            {
-                empHrs = 0;
+                case IS_FULL_TIME:
+                    empHrs = 8;
+                    break;
+                case IS_PART_TIME:
+                    empHrs = 4;
+                    break;
+                default:
+                    empHrs = 0;
+                    break;
             }
             empWage = empHrs * EMP_RATE_PER_HOUR;
             Console.WriteLine("Employee Wage is :" + empWage);
