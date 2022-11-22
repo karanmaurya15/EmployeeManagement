4eece3216376b1d296129834367bf043ed805e2c
karanmaurya15 <karanmaurya9663@gmail.com>
2022-11-22 20:14:28 +0530

    Added Part Time Employee

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -7,14 +7,19 @@
             Console.WriteLine("Welcome to Employee Wage Computation Program\n");
           
             int IS_FULL_TIME = 1;
+            int IS_PART_TIME = 2;
             int EMP_RATE_PER_HOUR = 150;
             int empHrs = 0;
             int empWage = 0;
             Random random = new Random();          
-            int empCheck = random.Next(0, 2);
+            int empCheck = random.Next(0, 3);
             if (empCheck == IS_FULL_TIME)
             {
                 empHrs = 8;
+            }
+            else if (empCheck == IS_PART_TIME)
+            {
+                empHrs = 4;
             }
             else
             {
