192c024549326edce6f281901f91b52902cafbb6
karanmaurya15 <karanmaurya9663@gmail.com>
2022-11-22 20:05:05 +0530

    Employee Daily Wage is Done

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -7,17 +7,23 @@
             Console.WriteLine("Welcome to Employee Wage Computation Program\n");
           
             int IS_FULL_TIME = 1;
-            Random random = new Random();
-           
+            int EMP_RATE_PER_HOUR = 150;
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();          
             int empCheck = random.Next(0, 2);
             if (empCheck == IS_FULL_TIME)
             {
-                Console.WriteLine("Employee is Present");
+                empHrs = 8;
             }
             else
             {
-                Console.WriteLine("Employee is Absent");
+                empHrs = 0;
             }
+            empWage = empHrs * EMP_RATE_PER_HOUR;
+            Console.WriteLine("Employee Wage is :" + empWage);
+            
+
         }
     }
 }
