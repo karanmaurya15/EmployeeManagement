4d11f7cbfa15e12943745094730e7f838b04784d
karankushwaha123 <karankushwaha123@gmail.com>
2022-11-22 19:50:52 +0530

    Employee Attendance is Done

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -4,7 +4,20 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program");
+            Console.WriteLine("Welcome to Employee Wage Computation Program\n");
+            //Constants
+            int IS_FULL_TIME = 1;
+            Random random = new Random();
+            //Computation
+            int empCheck = random.Next(0, 2);
+            if (empCheck == IS_FULL_TIME)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
         }
     }
 }
