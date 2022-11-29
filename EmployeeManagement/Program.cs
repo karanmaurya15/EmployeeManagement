a8c5ddc8a09db6b35a50a39b4803281a5d6ceadd
karanmaurya15 <karanmaurya9663@gmail.com>
2022-11-29 16:11:15 +0530

    Save the total Wage by Company

--- EmployeeManagement/Program.cs
+++ EmployeeManagement/Program.cs
@@ -2,44 +2,15 @@
 {
     internal class Program
     {
-        public const int IS_FULL_TIME = 1;
-        public const int IS_PART_TIME = 2;
-      
-        public static int ComputeEmpWage(string companyname, int empRatePerHour, int numOfWorkingDays, int maxHoursPerMonth)
-        {
-            int empHrs = 0;
-            int totalEmpHrs = 0;
-            int totalWorkingDays = 0;
-            while (totalEmpHrs <= maxHoursPerMonth && totalWorkingDays < numOfWorkingDays)
-            {
-                totalWorkingDays++;
-                Random random = new Random();
-                int empCheck = random.Next(0, 3);
-                switch (empCheck)
-                {
-                    case IS_FULL_TIME:
-                        empHrs = 8;
-                        break;
-                    case IS_PART_TIME:
-                        empHrs = 4;
-                        break;
-                    default:
-                        empHrs = 0;
-                        break;
-                }
-                totalEmpHrs += empHrs;
-                Console.WriteLine("Day : " + totalWorkingDays + "- Employee Hrs : " + empHrs);
-            }
-            int totalEmpWage = totalEmpHrs * empRatePerHour;
-            Console.WriteLine("\nTotal Emp Wage for company " + companyname + " is: " + totalEmpWage);
-            return totalEmpWage;
-        }
+        
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Employee Wage Computation Program\n");
-            ComputeEmpWage("Microsoft", 100, 20, 100);
-            Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
-            ComputeEmpWage("Google", 200, 20, 150);
+            EmpWageBuilderObject Microsoft = new EmpWageBuilderObject("Microsoft", 100, 20, 100);
+            EmpWageBuilderObject Google = new EmpWageBuilderObject("Google", 200, 30, 150);
+            Microsoft.computeEmpWage();
+            Console.WriteLine(Microsoft.toString());
+            Google.computeEmpWage();
+            Console.WriteLine(Google.toString());
 
         }
     }
